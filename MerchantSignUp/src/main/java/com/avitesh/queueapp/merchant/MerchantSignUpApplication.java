83f98772ea34ee5c4cad6f9dbdff73e593e0c910
akesharw <akesharw@LNAR-PC07WBAT.corp.capgemini.com>
2019-02-08 14:31:39 -0600

    initial project committ

---
+++ MerchantSignUp/src/main/java/com/avitesh/queueapp/merchant/MerchantSignUpApplication.java
@@ -0,0 +1,14 @@
+package com.avitesh.queueapp.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantSignUpApplication.class, args);
+	}
+
+}
+
